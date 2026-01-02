4a73c2f116cfe129f8488f2fe60e33321f0c60d7
GenshinMinecraft <gm@highp.ing>
2026-01-02 10:25:04 +0800

    feat: enhance monitoring data structures and implement system metrics retrieval

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+#![warn(clippy::all, clippy::pedantic)]
+#![allow(
+    clippy::cast_sign_loss,
+    clippy::cast_precision_loss,
+    clippy::cast_possible_truncation,
+    clippy::similar_names,
+    clippy::too_many_lines,
+    clippy::await_holding_lock,
+    dead_code
+)]
+
+use crate::monitoring::data_structure::MonitoringData;
+
 mod monitoring;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    loop {
+        let all = MonitoringData::refresh_and_get().await;
+        println!("{all:#?}");
+        println!("Size: {} Bytes", size_of_val(&all));
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
 }
--- src/monitoring/data_structure.rs
+++ src/monitoring/data_structure.rs
@@ -1,77 +1,96 @@
 // 若数据量字段中未注明单位，则以字节 (Bytes) 为单位
 // 若速度字段中未注明单位，则以字节每秒 (Bytes per second) 为单位
 
-struct MonitoringData {
-    cpu: CPUData,
-    ram: RamData,
-    load: LoadData,
-    system: SystemData,
-    disk: Vec<PerDiskData>,
-    network: Vec<PerNetworkInterfaceData>,
+#[derive(Debug)]
+pub struct MonitoringData {
+    pub cpu: CPUData,
+    pub ram: RamData,
+    pub load: LoadData,
+    pub system: SystemData,
+    pub disk: Vec<PerDiskData>,
+    pub network: Vec<PerNetworkInterfaceData>,
 }
 
-struct CPUData {
-    per_core: Vec<PerCpuCoreData>,
-    physical_cores: u64,
-    logical_cores: u64,
-    total_cpu_usage: f64,
+#[derive(Debug, Clone)]
+pub struct CPUData {
+    // 不变
+    pub physical_cores: u64,
+    pub logical_cores: u64,
+
+    // 变
+    pub per_core: Vec<PerCpuCoreData>,
+    pub total_cpu_usage: f64,
 }
 
-struct PerCpuCoreData {
-    name: String,
-    cpu_usage: f64,
-    frequency_mhz: u64,
-    vendor_id: String,
-    brand: String,
+#[derive(Debug, Clone)]
+pub struct PerCpuCoreData {
+    // 不变
+    pub name: String,
+    pub vendor_id: String,
+    pub brand: String,
+
+    // 变
+    pub cpu_usage: f64,
+    pub frequency_mhz: u64,
 }
 
-struct RamData {
-    total_memory: u64,
-    available_memory: u64,
-    used_memory: u64,
-
-    total_swap: u64,
-    available_swap: u64,
-    used_swap: u64,
+#[derive(Debug, Clone)]
+pub struct RamData {
+    // 变
+    pub total_memory: u64,
+    pub available_memory: u64,
+    pub used_memory: u64,
+    pub total_swap: u64,
+    pub used_swap: u64,
 }
 
-struct LoadData {
-    one: f64,
-    five: f64,
-    fifteen: f64,
+#[derive(Debug, Clone)]
+pub struct LoadData {
+    // 变
+    pub one: f64,
+    pub five: f64,
+    pub fifteen: f64,
 }
 
-struct SystemData {
-    system_name: String,
-    system_kernel: String,
-    system_kernel_version: String,
-    system_os_version: String,
-    system_os_long_version: String,
-    distribution_id: String,
-    system_host_name: String,
-    arch: String,
-    boot_time: u64,
-    uptime: u64,
-    process_count: u64,
+#[derive(Debug, Clone)]
+pub struct SystemData {
+    // 不变
+    pub system_name: String,
+    pub system_kernel: String,
+    pub system_kernel_version: String,
+    pub system_os_version: String,
+    pub system_os_long_version: String,
+    pub distribution_id: String,
+    pub system_host_name: String,
+    pub arch: String,
+
+    // 变
+    pub boot_time: u64,
+    pub uptime: u64,
+    pub process_count: u64,
 }
 
-struct PerDiskData {
-    kind: String, // e.g., SSD, HDD
-    name: String,
-    file_system: String,
-    mount_point: String,
-    total_space: u64,
-    available_space: u64,
-    is_removable: bool,
-    is_read_only: bool,
-    read_speed: u64,
-    write_speed: u64,
+#[derive(Debug, Clone)]
+pub struct PerDiskData {
+    // 变
+    pub kind: String, // e.g., SSD, HDD
+    pub name: String,
+    pub file_system: String,
+    pub mount_point: String,
+    pub total_space: u64,
+    pub available_space: u64,
+    pub is_removable: bool,
+    pub is_read_only: bool,
+    pub read_speed: u64,
+    pub write_speed: u64,
 }
 
-struct PerNetworkInterfaceData {
-    interface_name: String,
-    total_received: u64,
-    total_transmitted: u64,
-    receive_speed: u64,
-    transmit_speed: u64,
+#[derive(Debug, Clone)]
+pub struct PerNetworkInterfaceData {
+    // 变
+    pub interface_name: String,
+    pub total_received: u64,    // 从上次网卡重启开始计算
+    pub total_transmitted: u64, // 从上次网卡重启开始计算
+    pub receive_speed: u64,
+    pub transmit_speed: u64,
 }
