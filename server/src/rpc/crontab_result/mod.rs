79153d63cb1c821e03488a69d9a7447b7fc66a20
GenshinMinecraft <gm@highp.ing>
2026-04-30 23:49:34 +0800

    Feat: Update delete functionality to use query parameters instead of delete_params

--- server/src/rpc/crontab_result/mod.rs
+++ server/src/rpc/crontab_result/mod.rs
@@ -4,23 +4,12 @@ use jsonrpsee::core::RpcResult;
 use jsonrpsee::core::async_trait;
 use jsonrpsee::proc_macros::rpc;
 use nodeget_lib::crontab_result::query::CrontabResultDataQuery;
-use serde::Deserialize;
-use serde::Serialize;
 use serde_json::value::RawValue;
 use tracing::Instrument;
 
 mod auth;
 mod delete;
 mod query;
-
-/// `CrontabResult` 删除参数
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CrontabResultDelete {
-    /// 可选的 `cron_name` 过滤，若指定则只删除该 `cron_name` 的记录
-    pub cron_name: Option<String>,
-    /// 删除该时间之前的记录（毫秒时间戳）
-    pub before_time: i64,
-}
 
 #[rpc(server, namespace = "crontab-result")]
 pub trait Rpc {
@@ -32,7 +21,7 @@ pub trait Rpc {
     async fn delete(
         &self,
         token: String,
-        delete_params: CrontabResultDelete,
+        query: CrontabResultDataQuery,
     ) -> RpcResult<Box<RawValue>>;
 }
 
@@ -57,11 +46,11 @@ impl RpcServer for CrontabResultRpcImpl {
     async fn delete(
         &self,
         token: String,
-        delete_params: CrontabResultDelete,
+        query: CrontabResultDataQuery,
     ) -> RpcResult<Box<RawValue>> {
         let (tk, un) = token_identity(&token);
-        let span = tracing::info_span!(target: "crontab_result", "crontab-result::delete", token_key = tk, username = un, delete_params = ?delete_params);
-        async { rpc_exec!(delete::delete(token, delete_params).await) }
+        let span = tracing::info_span!(target: "crontab_result", "crontab-result::delete", token_key = tk, username = un, query = ?query);
+        async { rpc_exec!(delete::delete(token, query).await) }
             .instrument(span)
             .await
     }
